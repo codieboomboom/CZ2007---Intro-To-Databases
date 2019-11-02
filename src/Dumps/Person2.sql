43f13efcfd0ca30ab78d73aee1a32395aabccbd0
forelsz <48678192+forelsz@users.noreply.github.com>
2019-11-02 15:09:51 +0800

    Update Person2.sql

--- src/Dumps/Person2.sql
+++ src/Dumps/Person2.sql
@@ -1,5 +1,7 @@
 -- Person2(address, zip, city_name, state_name)
 
+
+-- Undergraduates
 INSERT INTO Person2 VALUES ('5 Tuas Avenue 3 BS Logistics Hub', '639405', 'Singapore', 'Singapore');
 INSERT INTO Person2 VALUES('79 Robinson Road #15-04 CPF Building', '068897', 'Singapore', 'Singapore');
 INSERT INTO Person2 VALUES('1 north bridge road 19-04 high street centre', '179094', 'Singapore', 'Singapore');
@@ -13,3 +15,11 @@ INSERT INTO Person2 VALUES('3820  Rosemont Avenue', '32901', 'Melbourne', 'Flori
 INSERT INTO Person2 VALUES('2664  Sampson Street', '80427', 'Central City', 'Colorado');
 INSERT INTO Person2 VALUES('3307  Coal Street', '33019', 'Hollywood', 'Florida');
 INSERT INTO Person2 VALUES('2130  Rinehart Road', '33169', 'Miami', 'Florida');
+
+-- Graduates 
+INSERT INTO Person2 VALUES('12 Tuas Avenue 6 BS Community Hub', '842136', 'Singapore', 'Singapore');
+INSERT INTO Person2 VALUES('Blk 123 Rivervale Street 2', '654842', 'Singapore', 'Singapore');
+INSERT INTO Person2 VALUES('Blk 234 Rivervale Crescent', '654213', 'Singapore', 'Singapore');
+INSERT INTO Person2 VALUES('5034 Ang Mo Kio Ind Pk 2 #01-337', '569537', 'Singapore', 'Singapore');
+INSERT INTO Person2 VALUES('896 Dunearn Road #05-03 Sime Darby Centre', '589472', 'Singapore', 'Singapore');
+INSERT INTO Person2 VALUES('26 Ayer Rajah Crescent Ayer Rajah Industrial Estate', '139944', 'Singapore', 'Singapore');
