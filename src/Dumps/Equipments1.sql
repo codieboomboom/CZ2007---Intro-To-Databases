6e92072b780e018abe56519e02e059d5b8427af8
skiddattle <47343794+skiddattle@users.noreply.github.com>
2019-11-02 20:34:58 +0800

    added equipments 2

---
+++ src/Dumps/Equipments1.sql
@@ -0,0 +1,113 @@
+--Equipments1(lab_school, lab_name, id, model_number, date_purchased)
+
+SET IDENTITY_INSERT Equipments1 ON;
+
+--items in scse swlab1 
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 1',0001,'10001','2019-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 1',0002,'10002','2019-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 1',0003,'10003','2019-02-01');
+
+--items in scse software lab 3
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 3',0001,'10001','2014-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 3',0002,'10002','2014-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Software Lab 3',0003,'10003','2014-02-01');
+
+
+--items in scse hwlab3
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Lab 3',0001,'10001','2018-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Lab 3',0002,'10002','2018-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Lab 3',0003,'10003','2018-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Lab 3',0004,'10006','2018-02-03');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Lab 3',0005,'10007','2018-02-03');
+
+
+--items in scse hpl
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Project Lab',0001,'10001','2009-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Project Lab',0002,'10002','2009-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Project Lab',0003,'10003','2009-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Project Lab',0004,'10006','2009-02-03');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Hardware Project Lab',0005,'10007','2009-02-03');
+
+
+--items in scbe computer lab 1
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Computer Lab 1',0001,'10001','2017-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Computer Lab 1',0002,'10002','2017-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Computer Lab 1',0003,'10003','2017-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Computer Lab 1',0004,'10004','2017-05-01');
+
+--items in nbs it laboratory 1
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 1',0001,'10003','2016-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 1',0002,'10009','2016-05-01');
+
+--items in nbs it laboratory 2
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 2',0001,'10003','2013-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 2',0002,'10009','2013-05-01');
+
+--items in nbs it laboratory 3
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 3',0001,'10003','2012-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','IT Laboratory 3',0002,'10009','2012-03-01');
+
+
+
+--items in HSS Com lab 1
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 1',0001,'10003','2012-08-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 1',0002,'10009','2012-08-01');
+
+--items in hss com lab 4
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 4',0001,'10003','2012-03-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 4',0002,'10009','2012-03-01');
+
+--items in hss com lab 5
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 5',0001,'10003','2012-04-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('HSS','Computer Lab 5',0002,'10009','2012-04-01');
+
+--items in MAE Maker Lab
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0001,'10005','2010-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0002,'10006','2010-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0003,'10007','2010-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0004,'10008','2010-03-03');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0005,'10009','2010-04-03');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','Maker Lab',0006,'10010','2010-05-03');
+
+--items in cee enviromental lab
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environmental Laboratory',0001,'10001','2013-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environmental Laboratory',0002,'10009','2013-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environmental Laboratory',0003,'10010','2013-02-01');
+
+
+
+--Graduates Lab
+
+--SCSE Data Management and Analytics Lab
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Data Management and Analytics Lab',0001,'10003','2001-02-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Data Management and Analytics Lab',0002,'10009','2001-03-01');
+
+
+--'SCSE', 'Computational Intelligence Lab'
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Computational Intelligence Lab',0001,'10001','2013-11-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Computational Intelligence Lab',0002,'10009','2013-11-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCSE','Computational Intelligence Lab',0003,'10010','2013-12-01');
+
+
+--'CEE', 'Environment Laboratory'
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environment Laboratory',0001,'10001','2003-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environment Laboratory',0002,'10009','2003-01-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('CEE','Environment Laboratory',0003,'10010','2003-05-01');
+
+
+
+--'NBS', 'Communication Laboratory 1'
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','Communication Laboratory 1',0001,'10003','2002-03-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('NBS','Communication Laboratory 1',0002,'10009','2002-03-01');
+
+
+--'SCBE', 'NTU Food Technology Centre'
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Communication Laboratory 1',0001,'10003','2002-03-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('SCBE','Communication Laboratory 1',0002,'10009','2002-03-01');
+
+
+--'MAE', 'ST Engineering-NTU Corporate Lab'
+
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','ST Engineering-NTU Corporate Lab',0001,'10001','2013-07-01');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','ST Engineering-NTU Corporate Lab',0002,'10002','2013-07-02');
+INSERT INTO Equipments1 (lab_school, lab_name, id, model_number, date_purchased) VALUES ('MAE','ST Engineering-NTU Corporate Lab',0003,'10003','2013-07-01');
---
+++ src/Dumps/Equipments2.sql
@@ -0,0 +1,12 @@
+-- Equipments 2 (model_number,name) model_number is primary key
+
+INSERT INTO Equipments2 VALUES ('10001', 'Screwdriver');
+INSERT INTO Equipments2 VALUES ('10002', 'Allen Key');
+INSERT INTO Equipments2 VALUES ('10003', 'Stopwatch');
+INSERT INTO Equipments2 VALUES ('10004', 'Multimeter');
+INSERT INTO Equipments2 VALUES ('10005', 'Oscilloscope');
+INSERT INTO Equipments2 VALUES ('10006', 'Power Supply');
+INSERT INTO Equipments2 VALUES ('10007', 'Function Generator');
+INSERT INTO Equipments2 VALUES ('10008', 'Breadboard');
+INSERT INTO Equipments2 VALUES ('10009', 'Printer');
+INSERT INTO Equipments2 VALUES ('10010', 'Pliers');
