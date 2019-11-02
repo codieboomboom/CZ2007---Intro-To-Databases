765324c06093e74e0e4e43b4707db4dd1420fc12
forelsz <48678192+forelsz@users.noreply.github.com>
2019-11-02 15:09:26 +0800

    Update Person1.sql

--- src/Dumps/Person1.sql
+++ src/Dumps/Person1.sql
@@ -1,5 +1,6 @@
 -- Person1(person_id, name, school, phone, email, address)
 
+--Undergraduates
 INSERT INTO Person1 VALUES ('S9278576G', 'Meng Peng', 'SCSE', '92837123', 'raymond.kassulke@gmail.com', '5 Tuas Avenue 3 BS Logistics Hub');
 INSERT INTO Person1 VALUES ('S9641234H', 'Jameson Hickle Sr.', 'SCSE', '84921783', 'oswald.boehm@hotmail.com', '1 north bridge road 19-04 high street centre');
 INSERT INTO Person1 VALUES ('G0384723F', 'Haskell Quitzon', 'NBS', '92351235', 'odell.bartoletti@bauch.com', '2317  New Creek Road');
@@ -13,3 +14,11 @@ INSERT INTO Person1 VALUES ('G0283746T', 'Kurt Stracke', 'HSS', '90876589', 'zul
 INSERT INTO Person1 VALUES ('G0385762H', 'Alfredo Walter', 'SCSE', '92836162', 'hershel72@yahoo.com', '2664  Sampson Street');
 INSERT INTO Person1 VALUES ('G8362519J', 'Electa Little', 'CEE', '98741366', 'hills.brielle@haley.net', '3307  Coal Street');
 INSERT INTO Person1 VALUES ('G0286804R', 'Antonina Robel', 'MAE', '84654162', 'dolly.bechtelar@jones.com', '2130  Rinehart Road');
+
+-- Graduates
+INSERT INTO Person1 VALUES('S9283742H', 'Ping  Qiu', 'SCSE', '91234512', '4renato.claudio.y@mercurecasino9.com', '12 Tuas Avenue 6 BS Community Hub');
+INSERT INTO Person1 VALUES('S9487271Y', 'Guanting  Cai', 'CEE', '88812373', 'amkahaled84i@suhusangar.ml', 'Blk 123 Rivervale Street 2');
+INSERT INTO Person1 VALUES('G0237589F', 'Bo  Chen', 'NBS', '80937616', 'kamr.ka@fodmaster21pbjp.cf', 'Blk 234 Rivervale Crescent');
+INSERT INTO Person1 VALUES('S9582767G', 'Wuhan  Tan', 'SCSE', '97863812', 'bayoub.craw.395e@chelseaartsgroup.com', '5034 Ang Mo Kio Ind Pk 2 #01-337');
+INSERT INTO Person1 VALUES('G0986589F', 'Fang  Wang', 'SCBE', '90283612', 'ighaithtallik@nygmkv.icu', '896 Dunearn Road #05-03 Sime Darby Centre');
+INSERT INTO Person1 VALUES('S9847267G', 'Xiuying  Chang', 'MAE', '89037162', 'rfati.moka@region13.ga', '26 Ayer Rajah Crescent Ayer Rajah Industrial Estate');
