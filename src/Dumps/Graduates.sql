c7b17ffa25911fb1e961aa600591ca142051b676
skiddattle <47343794+skiddattle@users.noreply.github.com>
2019-11-03 14:07:01 +0800

    made changes to research and graduates to match query

--- src/Dumps/Graduates.sql
+++ src/Dumps/Graduates.sql
@@ -1,8 +1,22 @@
 -- ssp5g1.dbo.Graduates(student_person_id, professor_person_id, research_topic)
 
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9283742H', 'S7625390H', 'Evolutionary and Memetic Computing');
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9487271Y', 'S6827369H', 'Coastal and Offshore Processes');
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0237589F', 'S8103746G', 'Marketting');
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9582767G', 'S6928369G', 'Computational Neural Modelling');
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0986589F', 'S6283764F', 'Process Systems Engineering');
-INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9847267G', 'S8647838F', 'Aerodynamic Performance of Joined-wing Single Aisle Aircrafts');
+--Computer Science(take course and research)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9283742H', 'S0385888H', 'Evolutionary and Memetic Computing');
+
+--Enviromental Engineering(research only)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9487271Y', 'S0283666T', 'Coastal and Offshore Processes');
+
+--Business(take course and research)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0237589F', 'S9412777H', 'Marketing');
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0237589F', 'S0385888H', 'Data Analytics');
+
+
+--Computer Engineering(take course and research)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9582767G', 'S0385888H', 'Computational Neural Modelling');
+
+--Biomedical Engineering(research only)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0986589F', 'S8362999J', 'Process Systems Engineering');
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('G0986589F', 'S0286111R', 'Thermal Conductivity');
+
+--Aerospace Engineering(take course only)
+INSERT INTO ssp5g1.dbo.Graduates VALUES ('S9847267G', 'S0286111R', NULL);
--- src/Dumps/Research.sql
+++ src/Dumps/Research.sql
@@ -1,8 +1,21 @@
 -- ssp5g1.dbo.Research(graduate_person_id, school, lab_name)
 
+--computer science
 INSERT INTO ssp5g1.dbo.Research VALUES ('S9283742H', 'SCSE', 'Data Management and Analytics Lab');
+
+--enviromental engineering
 INSERT INTO ssp5g1.dbo.Research VALUES ('S9487271Y', 'CEE', 'Environment Laboratory');
+
+--business, doing business and data analytics
 INSERT INTO ssp5g1.dbo.Research VALUES ('G0237589F', 'NBS', 'Communication Laboratory 1');
+INSERT INTO ssp5g1.dbo.Research VALUES ('G0237589F', 'SCSE', 'Computational Intelligence Lab');
+
+--computer engineering
 INSERT INTO ssp5g1.dbo.Research VALUES ('S9582767G', 'SCSE', 'Computational Intelligence Lab');
+
+--biomedical, doing process systems engineering and thermal conductivity
 INSERT INTO ssp5g1.dbo.Research VALUES ('G0986589F', 'SCBE', 'NTU Food Technology Centre');
+INSERT INTO ssp5g1.dbo.Research VALUES ('G0986589F', 'MAE', 'ST Engineering-NTU Corporate Lab');
+
+--aerospace
 INSERT INTO ssp5g1.dbo.Research VALUES ('S9847267G', 'MAE', 'ST Engineering-NTU Corporate Lab');
