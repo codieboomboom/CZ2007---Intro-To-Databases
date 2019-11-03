56ad38af36109e7b8ebfc82fa423406d61cf1a34
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2019-11-03 11:56:01 +0800

    Added semester information

--- src/Dumps/Person1.sql
+++ src/Dumps/Person1.sql
@@ -32,10 +32,12 @@ INSERT INTO ssp5g1.dbo.Person1 VALUES('S9582888G', 'Low Thia Kiang', 'SCSE', '97
 INSERT INTO ssp5g1.dbo.Person1 VALUES('S0965589F', 'Tan Ah Yew', 'SCBE', '90210212', 'tay0001@ntu.edu.sg', '222 Dunearn Road #05-03 Simei Derby Centre');
 INSERT INTO ssp5g1.dbo.Person1 VALUES('S9855567G', 'Darius Tan', 'MAE', '89047562', 'dt0001@ntu.edu.sg', '10 Crescent Telok Ayer');
 
---Stakeholders (not inserted into stakeholders table yet, might want to add to other tables)
+--Stakeholders (public)
 INSERT INTO ssp5g1.dbo.Person1 VALUES ('S9278545G', 'Bryan Tan', 'SCSE', '92837646', 'bt0002@ntu.edu.sg', '6 Boeing Avenue 3 BS Logistics Hub');
 INSERT INTO ssp5g1.dbo.Person1 VALUES ('S9641132H', 'Jameson Tan', 'SCSE', '84921324', 'jt0002@ntu.edu.sg', '79 Johnson Road #15-04 Poen Building');
 INSERT INTO ssp5g1.dbo.Person1 VALUES ('G0384069F', 'Shaun Lim', 'NBS', '92351864', 'sl0002@ntu.edu.sg', '5 East Bridge road 19-04 High street centre');
+
+--Stakeholders(goverment)
 INSERT INTO ssp5g1.dbo.Person1 VALUES ('S9472333H', 'Martyn Low', 'SCBE', ' 82736010', 'ml0002@ntu.edu.sg', '2 Malay Road 02-12');
 INSERT INTO ssp5g1.dbo.Person1 VALUES ('S9245444F', 'Starley Tee', 'SCSE', '90223888', 'st0002@ntu.edu.sg', '84 Tuas North Street 5 06-20');
 
--- src/Dumps/Stakeholders.sql
+++ src/Dumps/Stakeholders.sql
@@ -11,3 +11,12 @@ INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9223342H', 'Staff');
 INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9404971Y', 'Staff');
 INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S5893758F', 'Staff');
 
+--the public
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9278545G', 'Public');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9641132H', 'Public');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('G0384069F', 'Public');
+
+--the goverment
+
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9472333H', 'Goverment');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9245444F', 'Goverment');
