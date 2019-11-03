431f1e4828d88fe7f9d8bb1c94b25ec2eac8ca7d
Do Anh Tu <32509869+AnhTuDo1998@users.noreply.github.com>
2019-11-03 10:37:35 +0800

    Update queries from annex b

--- src/Dumps/Stakeholders.sql
+++ src/Dumps/Stakeholders.sql
@@ -1,13 +1,13 @@
 --Stakeholders (person_id, domain)
 
 --taken from first 3 students
-INSERT INTO Stakeholders VALUES ('S9278576G', 'Student');
-INSERT INTO Stakeholders VALUES ('S9641234H', 'Student');
-INSERT INTO Stakeholders VALUES ('G0384723F', 'Student');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES ('S9278576G', 'Student');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES ('S9641234H', 'Student');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES ('G0384723F', 'Student');
 
 
 --taken from first 3 staff
-INSERT INTO Stakeholders VALUES('S9223342H', 'Staff');
-INSERT INTO Stakeholders VALUES('S9404971Y', 'Staff');
-INSERT INTO Stakeholders VALUES('S5893758F', 'Staff');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9223342H', 'Staff');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S9404971Y', 'Staff');
+INSERT INTO ssp5g1.dbo.Stakeholders VALUES('S5893758F', 'Staff');
 
--- src/QUERY_DB.sql
+++ src/QUERY_DB.sql
@@ -1,16 +1,20 @@
 -- Question 1
-SELECT Person_ID
-FROM   Stakeholders
+SELECT person_id AS stakeholder_person_id
+FROM   ssp5g1.dbo.Stakeholders
 WHERE  Domain = 'Public'
 
 -- Question 2
-SELECT Person_ID, COUNT(Date_And_Time) AS CommentCount
-FROM   Comment_And_Suggestions
-WHERE  CommentCount >= 5
-GROUP BY Person_ID
+SELECT stakeholder_person_id
+FROM ssp5g1.dbo.CommentSuggestions
+GROUP BY stakeholder_person_id
+HAVING COUNT(date_time) >= 5
 
---Question 3 dunno how to do
-SELECT Student.Person_ID, COUNT(Professor.Person_ID) AS ProfCount,
+--Question 3 
+SELECT student_person_id
+FROM ssp5g1.dbo.Graduates AS G, ssp5g1.dbo.Research AS R
+WHERE G.student_person_id = R.graduate_person_id
+GROUP BY G.student_person_id
+HAVING COUNT(DISTINCT(professor_person_id)) >=1 AND COUNT(DISTINCT(school,lab_name)) >=1
 
 --Question 4
 SELECT Professor.Person_ID, COUNT(ID) AS CourseCount
@@ -19,16 +23,18 @@ WHERE  CourseCount > 1
 GROUP BY Professor.Person_ID
 
 -- Question 5
-SELECT ID
-FROM   Equipment
-WHERE  Lab.name = 'particular Laboratory'  --user input the particular lab name
+SELECT *
+FROM   ssp5g1.dbo.Equipments1 AS E1 , ssp5g1.dbo.Equipments2 As E2
+WHERE  E1.model_number = E2.model_number
+AND E1.lab_school = 'Particular school' AND E1.lab_name = 'Particular lab in the school'
 
 -- Question 6   dunno how to do
-SELECT Undergraduate.PersonID, COUNT(Date_And_Time) AS AttendanceCount  --cannot use Date
-FROM   Experiments
-WHERE  AttendanceCount
-GROUP BY Undergraduate.PersonID
-HAVING Attendance = 'p'
+SELECT undergraduate_person_id 
+FROM ssp5g1.dbo.Experiments AS E
+WHERE NOT EXISTS (SELECT undergraduate_person_id 
+					FROM ssp5g1.dbo.Experiments AS EX
+					WHERE attendance = 'P'
+					GROUP BY undergraduate_person_id)
 
 -- Question 7
 SELECT Graduate.PersonID 
